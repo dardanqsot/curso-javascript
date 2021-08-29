1242f75fb4c334c800eec83fc6af7cfb19dbdc5d
Dardan <quispesotodaniel@gmail.com>
2021-08-28 20:52:49 -0500

    Ecmascripts6 incio de curso

---
+++ ecmascript6+/ecmascript/src/es6/index.js
@@ -0,0 +1,102 @@
+//DEFAULT PARAMS Y CONCATENACION
+
+function newFunction(name, age, country) {
+    var name = name || 'darwin';
+    var age = age || 27;
+    var country = country || 'PE';
+    console.log(name, age, country);
+}
+
+
+//es6
+ function newFunction2(name = 'darwin', age = 27, country = "PE"){
+    console.log(name, age, country);
+ }
+
+ newFunction2();
+ newFunction2('Ricardo', 23, 'CO');
+
+ //TEMPLATE 
+
+ let hello = "Hello";
+ let world = "World";
+ let epicPhrase =  hello + ' '  + world;
+ console.log(epicPhrase);
+
+ let epicPhrase2 = `${hello} ${world}` //templates literals: comillas francesas
+ console.log(epicPhrase2);
+
+ let lorem = "ESTA ES UNA FRASE DE Ejemplo para probar multilinea con contraslash n \n"
+ + "como ejemplo este parrafo";
+
+ let lorem2 = `ESTA ES UNA FRASE DE Ejemplo para probar multilinea con contraslash n 
+ como ejemplo este parrafo`;
+
+ console.log(lorem);
+ console.log(lorem2);
+
+// desestructuracion
+ let person = {
+     'name': 'darwin',
+     'age': 27,
+     'country': 'PE'
+ }
+
+ console.log(person.name, person.age);
+
+ let {name, age, country} = person;
+
+ console.log(name, age, country);
+
+ //PROPAGACION
+ let team1 = ['darwin', 'Daniel', 'Juan'];
+ let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+ let education = ['David', ...team1, ...team2];
+
+ console.log(education);
+
+ //ARrow functions , Promesas y parámetros en objetos
+ let nombre = 'darwin';
+ let age = '27';
+
+ obj = { name: nombre, age : age};
+
+ obj2 = { nombre, age };
+
+ console.log( obj);
+
+ // arrow functions:
+
+ const names = [
+     {name: 'Darwin', age: 27},
+     {name: 'Yesisca', age: 25}
+ ]
+
+ let listOfNames = names.map( function(item){
+    console.log(item.name);
+ })
+
+ let listOfNames2 = names.map(item => console.log(item.name))
+/* funcion ejemplo a usar
+ const listOfNames3 = (name, age, country) => {
+     ...
+ }
+ */
+
+ //PROMESAS: ALGO VA A pasar
+  
+ const helloPromise = () => {
+     return new Promise((resolve, reject) => {
+        if(true){
+            resolve('Hey')
+        } else {
+            reject(' Ups!! ');
+        }
+     });
+ }
+
+ helloPromise()
+ .then(response => console.log(response))
+ .then(() => console.log('hola'))
+ .catch(error => console.log(error));
