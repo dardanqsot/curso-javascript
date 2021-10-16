70f9022edc816fca7ba82186b4762536fa462d21
darwinQSot <quispesotodaniel@gmail.com>
2021-10-15 23:46:28 -0500

    iniciando asyncronismo - callbacks

---
+++ fundamentos-nodeJS/async/callback.js
@@ -0,0 +1,10 @@
+function soyAsincrona() {
+    console.log('Hola, soy una función asicrona');
+}
+
+
+console.log('Iniciando proceso...');
+
+soyAsincrona();
+
+console.log('Terminando proceso ...');
