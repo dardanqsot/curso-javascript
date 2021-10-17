3b96fc98d105ca66ddd7c51a604d2733a3d40804
darwinQSot <quispesotodaniel@gmail.com>
2021-10-17 18:45:16 -0500

    Uso de AsyncAwait

---
+++ fundamentos-nodeJS/async/asyncAway.js
@@ -0,0 +1,34 @@
+async function hola(nombre,) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log("HOLA, " + nombre);
+            resolve(nombre);
+        },1500);
+    });
+}
+
+function hablar(nombre) {
+    return new Promise ( (resolve , reject) => {
+        setTimeout(function(){
+            console.log("Bla bla bla..");
+            resolve(nombre);
+        },1000)
+    })
+}
+
+function adios(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function () {
+            console.log("Adios, " + nombre);
+            resolve();
+        },1000);
+    });
+}
+
+async function main() {
+    let nombre = await hola('Darwin');
+    await hablar();
+    await adios(nombre);
+}
+
+main();
