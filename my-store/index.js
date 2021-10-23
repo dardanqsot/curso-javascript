aa08845c9119887d8e8a63da993d8c74bd23140b
darwinQSot <quispesotodaniel@gmail.com>
2021-10-22 21:12:55 -0500

    Routing en Exrpress

--- my-store/index.js
+++ my-store/index.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.json({
+    name: "Product 1",
+    price: 1000
+  });
+})
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 })
